87dc2fcb655cb7b419b9838a214266aea8941bd1
sathish <sathish19@gmail.com>
2026-09-09 13:35:47 +0530

    first commit

---
+++ tests/UITest.spec.js
@@ -0,0 +1,22 @@
+const {test} = require('@playwright/test')
+const {expect} = require('@playwright/test')
+
+test.describe.configure({mode:'parallel'})
+test('UITest', async({browser})=>{
+    const context = await browser.newContext()
+    const page = await context.newPage()
+    await page.goto("https://google.com/")
+    console.log(await page.title())
+    await expect(page).toHaveTitle('Google')
+});
+
+test('title', async({page})=>{
+    await page.goto("https://google.com/")
+    console.log(await page.title())
+})
+
+test('title2', async({page})=>{
+    await page.goto("https://google.com/")
+    console.log(await page.title())
+})
+
